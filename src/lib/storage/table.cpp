fca5538c55d67d76ed4f993cb50d1afb83237a1f
Niklas Riekenbrauck <nikriek@gmail.com>
2022-04-28 13:38:33 +0200

    Add another comment

--- src/lib/storage/table.cpp
+++ src/lib/storage/table.cpp
@@ -22,7 +22,7 @@ namespace opossum {
 Table::Table(const ChunkOffset target_chunk_size) : _target_chunk_size(target_chunk_size) { create_new_chunk(); }
 
 void Table::add_column(const std::string& name, const std::string& type) {
-  // We only allow adding columns for empty tables
+  // We only allow adding columns for empty tables to avoid dealing with default values
   Assert(row_count() == 0, "Adding a column is only allowed for empty tables");
   _column_names.push_back(name);
   _column_types.push_back(type);
